c6815fc8f7446a7e4a3e9d8a8beb04d1cae5c889
Zahra Alsayed <145253041+ZahraAlsayed@users.noreply.github.com>
2023-11-06 22:30:19 +0300

    Level 2 : Set up a server, GET requests to the root, L2 :Create a route to handle GET requests to /products

--- index.js
+++ index.js
@@ -1,10 +1,67 @@
-const http = require("http");
-const PORT = '8080'
+const http = require('http');
 
-http
-  .createServer((req, res) => {
-    /* handle http requests */
-  })
-  .listen(PORT);
+const port = 3001;
 
-console.log(`Server running at http://127.0.0.1:${PORT}/`);
+let products = [
+  { id: 1, name: "laptop", price: 3222 },
+  { id: 2, name: "Webcap", price: 1233 },
+  { id: 3, name: "Smart phone", price: 1233 }
+];
+
+const handelServserError = (res, statusCode, message) => {
+  res.writeHead(statusCode, { 'Content-Type': 'text/plain' });
+  res.write(message);
+  res.end();
+};
+
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  if (req.method === 'GET' && req.url === '/') {
+    try {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.write('Hello, World!\n');
+      res.end();
+    } catch (cerror) {
+      handelServserError(res, 500, 'Not Found');
+    }
+  } 
+  else if (req.method === 'POST' && req.url === '/') {
+     
+    try {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.write('Products is created Successfuly!\n');
+      res.end();
+    } catch (cerror) {
+      handelServserError(res, 500, 'Not Found');
+    }
+  } 
+
+  else if (req.method === 'GET' && req.url === '/products') {
+    try {
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.write(JSON.stringify(products));
+      res.end();
+    } catch (cerror) {
+      handelServserError(res, 500, 'Not Found');
+    }
+  } 
+  if (req.method === 'GET' && req.url.match(/\/products\/([0-9]+)/)) {
+    try {
+      const id = req.url?.split('/')[2];
+      const product = products.find((product) => product.id == id);
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.write(JSON.stringify(product));
+      res.end();
+    } catch (cerror) {
+      handelServserError(res, 500, 'Not Found');
+    }
+  } 
+
+});
+
+server.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
