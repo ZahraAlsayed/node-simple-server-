16559b4d40dc28e20e84586ffcc4a1a6a9706563
Zahra Alsayed <145253041+ZahraAlsayed@users.noreply.github.com>
2023-11-08 23:15:10 +0300

    DONE ALL L1 & L2

--- index.js
+++ index.js
@@ -1,62 +1,100 @@
+const { error } = require('console');
 const http = require('http');
+const { parse } = require('querystring');
+const fs = require('fs/promises');
 
 const port = 3001;
 
-let products = [
-  { id: 1, name: "laptop", price: 3222 },
-  { id: 2, name: "Webcap", price: 1233 },
-  { id: 3, name: "Smart phone", price: 1233 }
-];
 
 const handelServserError = (res, statusCode, message) => {
-  res.writeHead(statusCode, { 'Content-Type': 'text/plain' });
-  res.write(message);
-  res.end();
+  res.writeHead(statusCode, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify({
+    message : message,
+  }));
 };
 
-const server = http.createServer((req, res) => {
+const handelSuccessRespos = (res ,statusCode,message ,payload={}) => {
+  res.writeHead(statusCode, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify({
+    message: message,
+    payload: payload,
+    
+      }));
+};
+
+const server = http.createServer(async(req, res) => {
   console.log(req.url);
 
   res.setHeader('Access-Control-Allow-Origin', '*');
 
   if (req.method === 'GET' && req.url === '/') {
     try {
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.write('Hello, World!\n');
-      res.end();
+     handelSuccessRespos(res,200, "Hello, World!")
     } catch (cerror) {
-      handelServserError(res, 500, 'Not Found');
+      handelServserError(res, 500, error.message);
     }
-  } 
-  else if (req.method === 'POST' && req.url === '/') {
+  } else if (req.method === 'POST' && req.url === '/') {
+      try {
+        let body = '';
+        req.on('data', (chunk) => {
+          body = body + chunk;
+        })
+        req.on('end', (chunk) => {
+          body = body + chunk;
+          console.log(parse(body));
+        })
      
-    try {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.write('Products is created Successfuly!\n');
-      res.end();
-    } catch (cerror) {
-      handelServserError(res, 500, 'Not Found');
-    }
-  } 
-
+        handelSuccessRespos(res, 201, "Hello, World!");
+      } catch (cerror) {
+        handelServserError(res, 500, error.message);
+      }
+    
+  }
+  
   else if (req.method === 'GET' && req.url === '/products') {
+    const products =JSON.parse( await fs.readFile('products.json','utf-8'))
     try {
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.write(JSON.stringify(products));
-      res.end();
+      handelSuccessRespos(res,200, "Get All products" , products)
     } catch (cerror) {
-      handelServserError(res, 500, 'Not Found');
+      handelServserError(res, 500, error.message);
     }
   } 
-  if (req.method === 'GET' && req.url.match(/\/products\/([0-9]+)/)) {
+  else if (req.method === 'GET' && req.url.match(/\/products\/([0-9]+)/)) {
     try {
+       const products =JSON.parse( await fs.readFile('products.json','utf-8'))
       const id = req.url?.split('/')[2];
       const product = products.find((product) => product.id == id);
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.write(JSON.stringify(product));
-      res.end();
+     handelSuccessRespos(res,200, "Get one products information",product)
     } catch (cerror) {
-      handelServserError(res, 500, 'Not Found');
+      handelServserError(res, 500, error.message);
+    }
+  } 
+   else if (req.method === 'POST' && req.url === '/products') {
+    try {
+      let body = '';
+      req.on('data', (chunk) => {
+        body = body + chunk;
+      });
+      req.on('end', async() => {
+        const data = parse(body);
+        const newProduct = {
+          id: new Date().getTime().toString(),
+          name:data.name,
+          price:data.price,
+          
+        };
+        //get exisiteing data
+        const exisiteingProducts = JSON.parse(await fs.readFile('products.json', 'utf-8'))
+        // add the new product
+        exisiteingProducts.push(newProduct);
+        // write the file
+        await fs.writeFile('products.json',JSON.stringify(exisiteingProducts))
+
+        handelSuccessRespos(res, 201, "the product created sucessfuly");
+      });
+      
+    } catch (cerror) {
+      handelServserError(res, 500, error.message);
     }
   } 
 
